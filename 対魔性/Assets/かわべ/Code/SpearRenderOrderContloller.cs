7cdf6716af45cb23f2cc6516a0247e885763914d
FtoyCimpOmnkHty <55353405+FtoyCimpOmnkHty@users.noreply.github.com>
2019-12-30 15:47:27 +0900

    プレイヤーのアニメーション追加
    左向きの待機アニメーション

---
+++ 対魔性/Assets/かわべ/Code/SpearRenderOrderContloller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearRenderOrderContloller : MonoBehaviour
+{
+    [SerializeField] GameObject spear = null;
+    private SpriteRenderer[] SpearChildRenderers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpearChildRenderers = spear.GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetLayer(int order)
+    {
+        foreach (var cs in SpearChildRenderers)
+        {
+            cs.sortingOrder = order;
+        }
+    }
+}
