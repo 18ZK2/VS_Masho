adcc1ea61e05fc904fc3ed45bba8568d516d5803
FtoyCimpOmnkHty <55353405+FtoyCimpOmnkHty@users.noreply.github.com>
2019-12-30 11:31:08 +0900

    タイトルのゲームオブジェクト等追加
    対魔性/Assets/かわべ/Title.prefab
    がアニメーション用のゲームオブジェクトです

---
+++ 対魔性/Assets/かわべ/Code/PixenRenderBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixenRenderBase : MonoBehaviour
+{
+    private Vector3 cashPos;
+
+    private void LateUpdate()
+    {
+        cashPos = transform.localPosition;
+        transform.localPosition = new Vector3(
+            (int)cashPos.x,
+            (int)cashPos.y,
+            (int)cashPos.z);
+    }
+    private void OnRenderObject()
+    {
+        transform.localPosition = cashPos;
+    }
+}
