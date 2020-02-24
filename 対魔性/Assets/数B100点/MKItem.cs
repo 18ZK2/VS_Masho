28909f2a0abf89b1f55d17596efeefb166117e72
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-02-24 21:52:27 +0900

    Test

---
+++ 対魔性/Assets/数B100点/MKItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MKItem : MonoBehaviour
+{
+    GameObject Player;
+    PlayerContloller PlayerContloller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        PlayerContloller = Player.GetComponent<PlayerContloller>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            
+            PlayerContloller.MaxPlayerHp += 10;
+            Destroy(gameObject);
+        }
+
+    }
+}
