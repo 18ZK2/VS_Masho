813f0af19be37fa2fd3944204eaf599b4bed72a5
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-03-07 23:50:32 +0900

    ゲームオーバーシーン修正

--- 対魔性/Assets/数B100点/ゲームオーバーシーンプログラム/GameOverManager.cs
+++ 対魔性/Assets/数B100点/ゲームオーバーシーンプログラム/GameOverManager.cs
@@ -14,6 +14,14 @@ public class GameOverManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown) SceneManager.LoadScene("Map");
+        if (Input.GetKeyDown(KeyCode.R)) SceneManager.LoadScene("Map");
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            Application.Quit();
+            #endif
+        }
     }
 }
