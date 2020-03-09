cb787aef46e40252bc1419a55b28c28c0f11280b
poru0423 <46467086+poru0423@users.noreply.github.com>
2020-03-10 01:14:22 +0900

    ????

---
+++ 対魔性/Assets/かわべ/素材/プログラム/Enemy/enemyshot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyshot : MonoBehaviour
+{
+    [SerializeField] public Transform targetObj;
+    private float colliderOffset;
+    private Animator shoter = null;
+    public GameObject tumura;
+    float x, y, z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colliderOffset = GetComponent<CapsuleCollider>().radius + targetObj.GetComponent<CapsuleCollider>().radius;
+        shoter = GetComponent<Animator>();
+        Vector3 respown_t = GameObject.Find("発射機").transform.position;
+        x = respown_t.x;
+        y = respown_t.y;
+        z = respown_t.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(transform.position, targetObj.position) - colliderOffset;
+        Debug.Log(distance);
+
+        if (distance <= 400)
+        {
+            shoter.SetBool("distance", true);
+        }
+        else if (distance > 400)
+        {
+            shoter.SetBool("distance", false);
+        }
+    }
+    void animation_event()
+    {
+        for (int i = 0; i > 5; i++)
+        {
+            GameObject enemy = Instantiate(tumura);
+            enemy.transform.position = new Vector3(x, y, z);
+        }
+    }
+}
