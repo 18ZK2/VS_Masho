42f5485004cb0c252f418f9725d0e8f137f03be2
FtoyCimpOmnkHty <55353405+FtoyCimpOmnkHty@users.noreply.github.com>
2020-03-10 01:52:08 +0900

    enemyshotの不具合
    解決はしていないが、ヒントになりそうなことは書いた。頑張って。もう少し

--- 対魔性/Assets/かわべ/素材/プログラム/Enemy/enemyshot.cs
+++ 対魔性/Assets/かわべ/素材/プログラム/Enemy/enemyshot.cs
@@ -4,28 +4,33 @@ using UnityEngine;
 
 public class enemyshot : MonoBehaviour
 {
-    [SerializeField] public Transform targetObj;
-    private float colliderOffset;
+    [SerializeField] Transform targetObj;
+    private float colliderOffset = 0f;
     private Animator shoter = null;
     public GameObject tumura;
     float x, y, z;
     // Start is called before the first frame update
     void Start()
     {
-        colliderOffset = GetComponent<CapsuleCollider>().radius + targetObj.GetComponent<CapsuleCollider>().radius;
+        /*
+         * colliderOffsetで自身の当たり判定を引こうとしている？
+         */
+        //colliderOffset = GetComponent<CapsuleCollider>().radius + targetObj.GetComponent<CapsuleCollider>().radius;
         shoter = GetComponent<Animator>();
-        Vector3 respown_t = GameObject.Find("発射機").transform.position;
-        x = respown_t.x;
-        y = respown_t.y;
-        z = respown_t.z;
+        //Vector3 respown_t = GameObject.Find("発射機").transform.position;
+        //x = respown_t.x;
+        //y = respown_t.y;
+        //z = respown_t.z;
     }
 
     // Update is called once per frame
     void Update()
     {
         var distance = Vector3.Distance(transform.position, targetObj.position) - colliderOffset;
-        Debug.Log(distance);
-
+        //Debug.Log(distance);
+        /*
+         *測定範囲を可変にしたい場合、条件文の400の部分を変数にすればよい 
+         */
         if (distance <= 400)
         {
             shoter.SetBool("distance", true);
@@ -37,10 +42,15 @@ public class enemyshot : MonoBehaviour
     }
     void animation_event()
     {
-        for (int i = 0; i > 5; i++)
+        Debug.Log("Shot");
+        Instantiate(tumura, transform);
+        //五連続発射になるのでNG
+        //tumuraを入れる配列で管理するとよい
+        //倒されるとnullになるのでnullの数を数え、5未満なら生成など
+        /*for (int i = 0; i > 5; i++)
         {
-            GameObject enemy = Instantiate(tumura);
-            enemy.transform.position = new Vector3(x, y, z);
-        }
+            GameObject enemy = Instantiate(tumura,transform);
+            //enemy.transform.position = new Vector3(x, y, z);
+        }*/
     }
 }
