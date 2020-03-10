22c667b57f0ad3a122d587a49df2e4d9ce4cad86
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-03-10 15:47:28 +0900

    設定画面製作中

---
+++ 対魔性/Assets/数B100点/UIプログラム/ToggleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleController : MonoBehaviour
+{
+    [SerializeField] GameObject panel,SetOn;
+    bool Toggle=true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ToggleClicked()
+    {
+        this.gameObject.SetActive(false);
+        panel.SetActive(Toggle);
+        Toggle = !Toggle;
+        SetOn.SetActive(Toggle);
+    }
+}
