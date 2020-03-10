50a79f9bcc1e9ed8a9ccb35d9560ac8aace8940f
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-03-10 21:43:26 +0900

    設定画面3
    音量調整ができるように変更

---
+++ 対魔性/Assets/数B100点/UIプログラム/SoundOption.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundOption : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    Slider VolumeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VolumeSlider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void SetMaster()
+    {
+        audioMixer.SetFloat("MasterVol", VolumeSlider.value);
+    }
+
+    public void SetBGM()
+    {
+        audioMixer.SetFloat("BGMVol", VolumeSlider.value);
+    }
+
+    public void SetSE()
+    {
+        audioMixer.SetFloat("SEVol", VolumeSlider.value);
+    }
+}
