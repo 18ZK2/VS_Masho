6a0eca23b22c6ad329b5ff02c007e409f3e7238f
FtoyCimpOmnkHty <55353405+FtoyCimpOmnkHty@users.noreply.github.com>
2020-03-10 13:49:09 +0900

    調整2
    ステージ

---
+++ 対魔性/Assets/アカシ/Enemyshot.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyshot : MonoBehaviour
+{
+    const int MAX_ENEMY = 5;
+    [SerializeField] float distance_ani = 400f;
+    private Transform targetObj;
+    private Transform enemyList;
+    private Animator shoter = null;
+    private EnemyContloller ec;
+    public GameObject tumura;
+
+    int EnemyCount()
+    {
+        int count;
+        EnemyContloller[] childs = enemyList.GetComponentsInChildren<EnemyContloller>();
+        count = childs.Length;
+        return count;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetObj = GameObject.Find("Player").transform;
+        enemyList = transform.Find("EnemyList");
+        ec = GetComponent<EnemyContloller>();
+        shoter = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = 0f;
+        if(targetObj!=null)distance = Vector3.Distance(transform.position, targetObj.position);
+        Debug.Log(EnemyCount());
+        if (distance <= distance_ani && EnemyCount()<MAX_ENEMY)
+        {
+            shoter.SetBool("distance", true);
+        }
+        else if (distance > distance_ani || EnemyCount() > MAX_ENEMY)
+        {
+            shoter.SetBool("distance", false);
+        }
+        if (ec.HP <= 0)
+        {
+            //親が死ぬと子も死ぬので親が死ぬとき子を開放する
+            EnemyContloller[] childs = enemyList.GetComponentsInChildren<EnemyContloller>();
+            foreach (var child in childs) child.transform.parent = null;
+        }
+    }
+    void animation_event()
+    {
+        float x = transform.position.x - targetObj.position.x;
+        GameObject g = Instantiate(tumura, transform);
+        g.transform.parent = enemyList;
+        if (x < 0) g.transform.Rotate(0, 180, 0);
+    }
+}
