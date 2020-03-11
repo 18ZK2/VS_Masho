cae3eee543308f01482c849ea8fb638d6a6e9b3b
yuyujan <yu.main.address2@gmail.com>
2020-03-11 17:19:42 +0900

    ひよこの銃周り作成

---
+++ 対魔性/Assets/matsu/Hiyoko_ballet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hiyoko_ballet : MonoBehaviour
+{
+    public float Hiyo_ballet_damege = 1;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+    private void OnParticleCollision(GameObject obj)
+    {
+        Debug.Log("Hit!!");
+        if (obj.tag == "Player")
+        {
+            obj.GetComponent<PlayerContloller>().Damage(Hiyo_ballet_damege);
+        }
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 対魔性/Assets/matsu/Hiyoko_shot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hiyoko_shot : MonoBehaviour
+{
+    public GameObject bullet;
+    Animator ani;
+    // Start is called before the first frame update
+    private void Shot()
+    {
+        Instantiate(bullet, transform.position, transform.rotation);
+        ani.SetTrigger("Shot");
+    }
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Shot();
+        }
+    }
+}
