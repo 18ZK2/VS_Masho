ff6e7c9c1a29b11830fe06a2233b45bad2edf3d6
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-03-11 20:43:21 +0900

    ひよこ銃修正

--- 対魔性/Assets/matsu/Hiyoko_shot.cs
+++ 対魔性/Assets/matsu/Hiyoko_shot.cs
@@ -20,7 +20,11 @@ public class Hiyoko_shot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.parent.gameObject.GetComponent<EnemyContloller>().HP < 0) //親(Tumura)の体力が0未満になったとき
+        {
+            transform.parent = null; //親と関係を解除
+            Destroy(gameObject);
+        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
