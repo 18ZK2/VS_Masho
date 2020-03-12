749ecd5fbc13e8a1bc9cf429a27b2c7ccba70429
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-03-12 21:43:21 +0900

    敵の頭上のHPバーを作成

---
+++ 対魔性/Assets/数B100点/EnemyHpController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHpController : MonoBehaviour
+{   
+    GameObject Enemy;
+    EnemyContloller EnemyContloller;
+    Slider slider;
+    bool rot = true;
+    Vector3 def;
+    // Start is called before the first frame update
+    void Start()
+    {
+        def = transform.parent.transform.localRotation.eulerAngles;
+        Enemy = transform.root.gameObject; //親オブジェクト取得
+        EnemyContloller = Enemy.GetComponent<EnemyContloller>();
+        slider = GameObject.Find("EnemyHealth").GetComponent<Slider>();
+        slider.maxValue = EnemyContloller.HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 parent = transform.root.transform.localRotation.eulerAngles;
+        transform.parent.transform.localRotation = Quaternion.Euler(def - parent); //子が回転しないよう調整
+
+        slider.value = EnemyContloller.HP; //EnemyControllerからHPを参照
+    }
+}
