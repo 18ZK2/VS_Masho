3bc8a88e819d4bfa09474a9be1df85679f051702
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-03-14 23:47:50 +0900

    追従つむら作成

--- 対魔性/Assets/数B100点/FlyingTumuraController.cs
+++ 対魔性/Assets/数B100点/FlyingTumuraController.cs
@@ -4,26 +4,44 @@ using UnityEngine;
 
 public class FlyingTumuraController : MonoBehaviour
 {
-    Vector2 pos;
-    [SerializeField] float dis = 100.0f, dis2 = 150.0f;
+    [Header("追従")][SerializeField] bool homing = false; //追従する場合
+    Vector2 pos,diff;
+    [Header("地上からの距離")][SerializeField] float dis = 100.0f;
+    [Header("追従の力の大きさ")] [SerializeField] float f = 13.0f;
+
     float ve;
     TumuraContloller TumuraContloller;
     Rigidbody2D rb;
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player");
         TumuraContloller = GetComponent<TumuraContloller>();
         rb = GetComponent<Rigidbody2D>();
         ve = -(TumuraContloller.walkspeed / (rb.mass * rb.drag)*1.1f); //最高速度の1.1倍
     }
     private void FixedUpdate()
     {
-        
+        if (homing)
+        {
+            diff = (player.transform.position-transform.position); //TumuraからPlayerまでのベクトル
+            rb.AddForce(diff*f);
+            if (diff.x > 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+
+            }
+        }
         if (transform.eulerAngles.y == 180 && ve < 0 || transform.eulerAngles.y == 0 && ve > 0) //Tumuraが右向きかつveがマイナス または Tumuraが左向きかつveがプラス
         {
             ve = -ve;
         }
-        pos = new Vector2(ve, -dis2);
+        pos = new Vector2(ve, -1); //反転用の向きを作成
         Vector2 origin = new Vector2(transform.position.x, transform.position.y - 16.5f); //FlyingTumuraの場所
         RaycastHit2D hit1 = Physics2D.Raycast(origin, Vector2.down, dis, LayerMask.GetMask("Stage"));
         RaycastHit2D hit2 = Physics2D.Raycast(origin, pos, pos.magnitude*1.2f, LayerMask.GetMask("Stage")); //回転判定用
@@ -35,7 +53,7 @@ public class FlyingTumuraController : MonoBehaviour
         {
             rb.AddForce(new Vector2(0.0f, 1.0f) * rb.mass * rb.gravityScale * 15f, ForceMode2D.Force); //重力を打ち消す
         }
-        if (hit2.collider == null)
+        if (hit2.collider == null && !homing)
         {
             TumuraContloller.Turn();
         }
