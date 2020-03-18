409008e2ed1036a7e5ac7e168791fde45768e3bc
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-03-19 00:07:11 +0900

    ファイアバー作成?

---
+++ 対魔性/Assets/数B100点/FireController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    PlayerContloller player;
+    [SerializeField] GameObject thisgo;
+    [Header("個数")] public int n;
+    [Header("ダメージ")] [SerializeField] float damage = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerContloller>();
+        for (int i = 0; i < n; i++)
+        {
+            GameObject gameObject = Instantiate(thisgo, this.transform.position, Quaternion.identity) as GameObject;
+            FireController fireController = gameObject.GetComponent<FireController>(); //生成したゲームオブジェクトのfirecontroller取得
+            gameObject.transform.parent = this.gameObject.transform; //自分の子供にする
+            gameObject.transform.localPosition = new Vector3(20.0f * (i + 1), 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("good");
+        if (collision.tag == "Player")
+        {
+            player.PlayerHp -= damage;
+        }
+    }
+}
---
+++ 対魔性/Assets/数B100点/FireRot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireRot : MonoBehaviour
+{
+    bool col = true,rot=false;
+    float sleeptime;
+    [SerializeField] float rotspeed = 5.0f;
+    IEnumerator Sleep()
+    {
+        for (int i = 0; i < sleeptime; i++)
+        {
+            yield return null;
+        }
+        rot = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        sleeptime = GetComponent<FireController>().n;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (col == true) //一度だけコルーチンを実行
+        {
+            StartCoroutine("Sleep");
+            col = !col;
+        }
+        if (rot == true)
+        {
+            transform.Rotate(0,0,rotspeed);
+        }
+    }
+}
