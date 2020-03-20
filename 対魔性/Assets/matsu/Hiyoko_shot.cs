fae95a9d2012c8d0e82c8f90cc58afd873a1be4d
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-03-20 20:18:26 +0900

    スナイパー作成

--- 対魔性/Assets/matsu/Hiyoko_shot.cs
+++ 対魔性/Assets/matsu/Hiyoko_shot.cs
@@ -9,9 +9,10 @@ public class Hiyoko_shot : MonoBehaviour
     [SerializeField] float dis = 200.0f;
     public GameObject bullet,player;
     [SerializeField] AudioClip shotSE = null;
+    [Header("回転させる場合はRotOriginを代入")][SerializeField] GameObject RotOrigin;
     Animator ani;
     AudioSource ass;
-    [SerializeField] bool rot = false; //銃を回転させたい場合はtrue
+    [Header("回転")][SerializeField] bool rot = false; //銃を回転させたい場合はtrue
     // Start is called before the first frame update
     private void Shot()
     {
@@ -20,18 +21,29 @@ public class Hiyoko_shot : MonoBehaviour
     }
     void Start()
     {
+        if (rot == true) //回転軸を作成
+        {
+            GameObject ro = Instantiate(RotOrigin, this.transform.position, Quaternion.identity) as GameObject;
+            ro.transform.parent = transform.root; //RotOriginをTumuraの子にする
+            gameObject.transform.parent = ro.transform; //GunをRotOriginの子にする
+            ro.transform.localPosition= new Vector3(0, 0, 0);
+            transform.localPosition = new Vector3(-10.0f,0,0);
+        }
         player = GameObject.Find("Player");
         ani = GetComponent<Animator>();
         ass = GetComponent<AudioSource>();
     }
     private void FixedUpdate()
     {
-        Vector2 origin = new Vector2(transform.position.x, transform.position.y); //FlyingTumuraの場所
-        RaycastHit2D hit = Physics2D.Raycast(origin, (-transform.right),dis, LayerMask.GetMask("Player")); //判定
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y); //Tumuraの場所
+        RaycastHit2D hit = Physics2D.Raycast(origin, (-transform.right),dis); //判定(当たらなければNull)
         Debug.DrawRay(origin, dis*(-transform.right), Color.blue, 0.1f);
-        if (hit.collider) //プレイヤーを発見
+        if (hit.collider != null)
         {
-            ani.SetTrigger("Shot");
+            if (hit.collider.tag == "Player") //プレイヤーを発見
+            {
+                ani.SetTrigger("Shot");
+            }
         }
     }
     // Update is called once per frame
@@ -40,12 +52,28 @@ public class Hiyoko_shot : MonoBehaviour
         
         if (rot == true)
         {
-            var vec = (-transform.position + player.transform.position).normalized;
-            transform.parent.transform.rotation = Quaternion.FromToRotation(Vector3.up, vec)*Quaternion.Euler(0,0,-90);
+            if (player != null)
+            {
+                var vec = (-transform.root.position + player.transform.position).normalized; //プレイヤーからTumuraへのベクトル
+                if (vec.x > 0)
+                {
+
+                    transform.root.rotation = Quaternion.Euler(new Vector3(0, 180, 0)); //Tumuraを180°回転
+                    transform.parent.transform.rotation = Quaternion.FromToRotation(Vector3.up, vec) * Quaternion.Euler(0, 180, -90);
+
+                }
+                else
+                {
+                    transform.root.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                    transform.parent.transform.rotation = Quaternion.FromToRotation(Vector3.up, vec) * Quaternion.Euler(0, 0, -90);
+                }
+                //transform.parent.Rotate(0, 0, 1.0f);
+
+            }
         }
-        if (transform.parent.gameObject.GetComponent<EnemyContloller>().HP < 0) //親(Tumura)の体力が0未満になったとき
+        if (transform.root.gameObject.GetComponent<EnemyContloller>().HP < 0) //親(Tumura)の体力が0未満になったとき
         {
-            transform.parent = null; //親と関係を解除
+            transform.parent.parent = null; //親と関係を解除
             Destroy(gameObject);
         }
     }
