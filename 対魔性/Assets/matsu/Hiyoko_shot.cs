096bb82979400656e1f5397bcfe1cf88790072d4
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-03-20 21:21:57 +0900

    GunTumuraのバグ修正

--- 対魔性/Assets/matsu/Hiyoko_shot.cs
+++ 対魔性/Assets/matsu/Hiyoko_shot.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Hiyoko_shot : MonoBehaviour
 {
+    EnemyContloller EnemyContloller;
     Vector3 pos, pla;
     Quaternion quaternion;
     [SerializeField] float dis = 200.0f;
     public GameObject bullet,player;
     [SerializeField] AudioClip shotSE = null;
+    [Header("回転")] [SerializeField] bool rot = false; //銃を回転させたい場合はtrue
     [Header("回転させる場合はRotOriginを代入")][SerializeField] GameObject RotOrigin;
     Animator ani;
     AudioSource ass;
-    [Header("回転")][SerializeField] bool rot = false; //銃を回転させたい場合はtrue
     // Start is called before the first frame update
     private void Shot()
     {
@@ -21,12 +22,13 @@ public class Hiyoko_shot : MonoBehaviour
     }
     void Start()
     {
-        if (rot == true) //回転軸を作成
+        EnemyContloller = transform.parent.gameObject.GetComponent<EnemyContloller>(); //if rotより先に書く(親が変わるため)
+        if (rot == true) //回転させるとき
         {
-            GameObject ro = Instantiate(RotOrigin, this.transform.position, Quaternion.identity) as GameObject;
-            ro.transform.parent = transform.root; //RotOriginをTumuraの子にする
+            GameObject ro = Instantiate(RotOrigin, this.transform.position, Quaternion.identity) as GameObject; //回転軸(プレハブ)を作成
+            ro.transform.parent = transform.parent; //RotOriginをTumuraの子にする
             gameObject.transform.parent = ro.transform; //GunをRotOriginの子にする
-            ro.transform.localPosition= new Vector3(0, 0, 0);
+            ro.transform.localPosition= new Vector3(0, 0, 0); //回転軸を
             transform.localPosition = new Vector3(-10.0f,0,0);
         }
         player = GameObject.Find("Player");
@@ -49,33 +51,38 @@ public class Hiyoko_shot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         if (rot == true)
         {
             if (player != null)
             {
-                var vec = (-transform.root.position + player.transform.position).normalized; //プレイヤーからTumuraへのベクトル
+                var vec = (player.transform.position - transform.parent.parent.position).normalized; //プレイヤーからTumuraへのベクトル parent.parentにする!!!!!(rootではだめ)
                 if (vec.x > 0)
                 {
 
-                    transform.root.rotation = Quaternion.Euler(new Vector3(0, 180, 0)); //Tumuraを180°回転
+                    transform.parent.parent.rotation = Quaternion.Euler(new Vector3(0, 180, 0)); //Tumuraを180°回転
                     transform.parent.transform.rotation = Quaternion.FromToRotation(Vector3.up, vec) * Quaternion.Euler(0, 180, -90);
 
                 }
                 else
                 {
-                    transform.root.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                    transform.parent.parent.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
                     transform.parent.transform.rotation = Quaternion.FromToRotation(Vector3.up, vec) * Quaternion.Euler(0, 0, -90);
                 }
-                //transform.parent.Rotate(0, 0, 1.0f);
-
             }
         }
-        if (transform.root.gameObject.GetComponent<EnemyContloller>().HP < 0) //親(Tumura)の体力が0未満になったとき
-        {
-            transform.parent.parent = null; //親と関係を解除
-            Destroy(gameObject);
-        }
+        //if (EnemyContloller!= null)
+        //{
+        //    if (EnemyContloller.HP <= 0) //親(Tumura)の体力が0以下になったとき
+        //    {
+        //        Debug.Log("g");
+        //        transform.parent = null; //親と関係を解除
+        //        Destroy(gameObject);
+        //    }
+        //}
+        //else
+        //{
+        //    Debug.Log("ありえない話し！");
+        //}
     }
     
 }
