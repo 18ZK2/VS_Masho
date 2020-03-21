f5c79a96769216cfc25c464b2ad3665934e25b1b
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-03-21 23:48:18 +0900

    Timer追加+α
    タイトルとゲームオーバーシーンを少し修正

---
+++ 対魔性/Assets/数B100点/UIプログラム/TimeCount.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCount : MonoBehaviour
+{
+    Text timerText;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        timerText.text = "Time:" + Mathf.Ceil(timer * 1000.0f)/1000.0f;
+    }
+}
