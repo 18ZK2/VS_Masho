1315374debfc1de2e0305428be1ece3d238315c0
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-03-24 00:46:08 +0900

    バグ修正
    FlyingTumura

--- 対魔性/Assets/数B100点/FlyingTumuraController.cs
+++ 対魔性/Assets/数B100点/FlyingTumuraController.cs
@@ -4,68 +4,45 @@ using UnityEngine;
 
 public class FlyingTumuraController : MonoBehaviour
 {
-    [Header("追従")][SerializeField] bool homing = false; //追従する場合
-    Vector2 pos,diff;
+    public HomingTumura homing;
     [Header("地上からの距離")][SerializeField] float dis = 100.0f;
-    [Header("追従の力の大きさ")] [SerializeField] float f = 13.0f;
-
-    float ve,maxf=110.0f;
+    bool exit = true;
     TumuraContloller TumuraContloller;
     Rigidbody2D rb;
-    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
+        homing = GetComponent<HomingTumura>();
         TumuraContloller = GetComponent<TumuraContloller>();
         rb = GetComponent<Rigidbody2D>();
         //ve = -(TumuraContloller.walkspeed / (rb.mass * rb.drag)*1.1f); //最高速度の1.1倍
     }
     private void FixedUpdate()
     {
-        if (homing && player!=null)
-        {
-            diff = (player.transform.position - transform.position); //TumuraからPlayerまでのベクトル
-            
-            if (diff.magnitude<2000.0f) //一定範囲内の場合
-            {
-                if (diff.magnitude > maxf) 
-                {
-                    diff = diff.normalized*maxf; 
-                }
-                rb.AddForce(diff * f);
-                if (diff.x > 0)
-                {
-                    transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
-                }
-                else
-                {
-                    transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-
-                }
-            }
-        }
         //if (transform.eulerAngles.y == 180 && ve < 0 || transform.eulerAngles.y == 0 && ve > 0) //Tumuraが右向きかつveがマイナス または Tumuraが左向きかつveがプラス
         //{
         //    ve = -ve;
         //}
        // pos = new Vector2(ve, -dis); //反転用の向きを作成
-        Vector2 Forigin = new Vector2(transform.position.x, transform.position.y - 16.5f); //FlyingTumuraの場所(浮遊用)
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y); //FlyingTumuraの場所(浮遊用)
         Vector2 Rorigin = new Vector2(transform.position.x+transform.right.x*-10.0f, transform.position.y - 16.5f); //FlyingTumuraの場所(回転用)
-        RaycastHit2D hit1 = Physics2D.Raycast(Forigin, Vector2.down, dis, LayerMask.GetMask("Stage"));
+        RaycastHit2D hit1 = Physics2D.Raycast(origin, Vector2.down, dis, LayerMask.GetMask("Stage"));
         //RaycastHit2D hit2 = Physics2D.Raycast(origin, pos, pos.magnitude*1.2f, LayerMask.GetMask("Stage")); //回転判定用
         RaycastHit2D hit2 = Physics2D.Raycast(Rorigin, Vector2.down, dis * 2.1f, LayerMask.GetMask("Stage")); //回転判定用
-        Debug.DrawRay(Forigin,new Vector2(0,-dis),Color.red,0.1f);
+        Debug.DrawRay(origin,new Vector2(0,-dis),Color.red,0.1f);
         //Debug.DrawRay(origin, pos*1.2f, Color.yellow, 0.1f);
         Debug.DrawRay(Rorigin, Vector2.down*2.1f*dis, Color.yellow, 0.1f);
-
-
         if (hit1.collider)
         {
             rb.AddForce(new Vector2(0.0f, 1.0f) * rb.mass * rb.gravityScale * 16f, ForceMode2D.Force); //重力を打ち消す
         }
-        if (hit2.collider == null && !homing) //homingのときは使わない
+        if (hit2.collider &&homing==null)
         {
+            exit = hit2;
+        }
+        else if (exit == true && homing==null &&!CompareTag("PlayerAttack")) //homingのときは使わない 一度だけ実行(OnCollisionExitみたいな)
+        {
+            exit = hit2;
             rb.velocity = new Vector2(0.0f,rb.velocity.y); //速度を0
             TumuraContloller.Turn();
         }
---
+++ 対魔性/Assets/数B100点/HomingTumura.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingTumura : MonoBehaviour
+{
+    [Header("追従の力の大きさ")] [SerializeField] float f = 13.0f;
+    [Header("0～360でオナシャス")] [SerializeField] float startangle = 0, finishangle = 90.0f;
+    Vector2 diff,hitvec;
+    float ve, maxf = 110.0f, mag=10.0f, angle;
+    bool dig = false;
+    TumuraContloller TumuraContloller;
+    Rigidbody2D rb;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        angle = startangle;
+        player = GameObject.Find("Player");
+        TumuraContloller = GetComponent<TumuraContloller>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        hitvec.x = mag*Mathf.Cos(angle * Mathf.Deg2Rad);
+        hitvec.y = mag*Mathf.Sin(angle * Mathf.Deg2Rad);
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y); //FlyingTumuraの場所(浮遊用)
+        RaycastHit2D hit = Physics2D.Raycast(origin, -transform.right*hitvec, mag, LayerMask.GetMask("Stage")); //
+        Debug.DrawRay(origin, transform.right.x * -mag*hitvec.normalized, Color.black, 0.1f);
+
+        if (player != null)
+        {
+            diff = (player.transform.position - transform.position); //TumuraからPlayerまでのベクトル
+            if (diff.magnitude < 2000.0f) //一定範囲内の場合
+            {
+                if (diff.magnitude > maxf) diff = diff.normalized * maxf;
+                rb.AddForce(diff * f);
+                if (diff.x > 0) transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+                else transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            }
+            if (hit.collider)
+            {
+                if (diff.y > 0) rb.AddForce(Vector2.up * 250.0f, ForceMode2D.Impulse);
+                else rb.AddForce(Vector2.down * 250.0f, ForceMode2D.Impulse);
+            }
+        }
+        //hit3の回転
+        {
+            angle += 2;
+            if (dig && angle > finishangle)
+            {
+                angle = startangle;
+                dig = false;
+            }
+            if (angle > 360.0f)
+            {
+                angle -= 360.0f;
+                dig = true;
+            }
+            Debug.Log(angle);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
