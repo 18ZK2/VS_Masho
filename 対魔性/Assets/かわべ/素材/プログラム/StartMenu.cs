af5dbf838cede75b65f31535b44cbc5e7b78fb26
FtoyCimpOmnkHty <55353405+FtoyCimpOmnkHty@users.noreply.github.com>
2020-03-23 19:19:44 +0900

    バグ修正
    タイトルのボタンのバグ

--- 対魔性/Assets/かわべ/素材/プログラム/StartMenu.cs
+++ 対魔性/Assets/かわべ/素材/プログラム/StartMenu.cs
@@ -6,10 +6,13 @@ public class StartMenu : MonoBehaviour
 {
     [SerializeField] string sceneName = "Map";
     GameManager gm;
+    bool isPlayed = false;
 
     public void GoToScene()
     {
+        if (isPlayed) return;
         gm = gameObject.AddComponent<GameManager>();
         StartCoroutine(gm.WipeLoadScene(sceneName));
+        isPlayed = true;
     }
 }
