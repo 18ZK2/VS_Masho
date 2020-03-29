b2df9508f67dba150d09a600c7908873d1d8966a
Rkr25 <59259835+Rkr25@users.noreply.github.com>
2020-03-29 22:58:25 +0900

    valcan完成

--- 対魔性/Assets/数B100点/プログラム/Valcan/ValcanController.cs
+++ 対魔性/Assets/数B100点/プログラム/Valcan/ValcanController.cs
@@ -8,7 +8,7 @@ public class ValcanController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 10.0f);
+        Destroy(gameObject, 3.0f);
     }
 
     // Update is called once per frame
--- 対魔性/Assets/数B100点/プログラム/Valcan/ValcanCreater.cs
+++ 対魔性/Assets/数B100点/プログラム/Valcan/ValcanCreater.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class ValcanCreater : MonoBehaviour
 {
+    float angle, y = 50.0f;
+    Vector2 rotvec;
     bool isLaunch = true;
-    [Header("回数")][SerializeField] int n = 3;
-    [SerializeField] float f = 100.0f;
+    [Header("連射回数")][SerializeField] int n = 3;
+    [Header("パワー")][SerializeField] float f = 2000.0f;
+    [Header("連射間隔")] [SerializeField] float delay = 0.2f;
+    [Header("次に撃つまでの間隔")] [SerializeField] float delayN = 0.5f;
+
     [SerializeField] GameObject Valcan=null;
     // Start is called before the first frame update
     public IEnumerator Createvalcan()
     {
         int i = 0;
+        //n回連射
         while (i < n)
         {
-            float rand = Random.Range(-0.8f, 0.8f); //乱数生成
-            GameObject preval = Instantiate(Valcan, 　new Vector2(transform.position.x, transform.position.y), Quaternion.identity) as GameObject;
-            preval.GetComponent<Rigidbody2D>().AddForce(-transform.right.x * f* new Vector2(1,rand), ForceMode2D.Impulse);
+            if (transform.rotation.eulerAngles.y == 0) angle = transform.rotation.eulerAngles.z+180.0f;//Euler角から変換
+            else angle = -transform.rotation.eulerAngles.z; //y==180の時はEulerのz=-zとなる
+            //角度からベクトルを作成
+            rotvec.x = y*Mathf.Cos(angle * Mathf.Deg2Rad);
+            rotvec.y = y*Mathf.Sin(angle * Mathf.Deg2Rad);
+            float rand = Random.Range(-15f, 15f); //乱数生成(15°から-15°まで回転)
+            GameObject preval = Instantiate(Valcan,new Vector2(transform.position.x+rotvec.x, transform.position.y-5.0f+rotvec.y), Quaternion.identity) as GameObject;
+            var vec = Quaternion.Euler(0, 0, rand) * rotvec;//かける順番あり(逆はだめ)
+            preval.GetComponent<Rigidbody2D>().AddForce(f * vec, ForceMode2D.Impulse);
             i++;
-            yield return new WaitForSeconds(.2f);
+            yield return new WaitForSeconds(delay);
         }
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(delayN);
         isLaunch = true;
     }
     void Start()
