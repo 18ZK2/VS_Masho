060ba1c6c1c23059e5f93e7e240024760274fb42
FtoyCimpOmnkHty <55353405+FtoyCimpOmnkHty@users.noreply.github.com>
2020-03-29 23:58:37 +0900

    ボス足調整+頭作成

---
+++ 対魔性/Assets/かわべ/素材/プログラム/Enemy/BossFootController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFootController : MonoBehaviour
+{
+    public bool isLaunch = false;
+    [SerializeField] float jointPow = 10000f;
+    [SerializeField] HingeJoint2D footJoint = null;
+    [SerializeField] Transform jointTrans;
+
+    JointMotor2D motor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = footJoint.motor;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float l = isLaunch ? 1 : 0;
+        float l_R = !isLaunch ? 1 : 0;
+        motor.motorSpeed = jointPow * l;
+        motor.motorSpeed = jointPow * l_R * -jointTrans.rotation.eulerAngles.z / 20f;
+        footJoint.motor = motor;
+    }
+}
---
+++ 対魔性/Assets/かわべ/素材/プログラム/Enemy/BossHeadController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHeadController : MonoBehaviour
+{
+    public bool isLaunch = false;
+    [SerializeField] float launchPow = 200f;
+    [SerializeField] GameObject gravitiBall = null, launchPoint = null;
+
+    bool sleeped = false;
+    IEnumerator Launch()
+    {
+        while (true)
+        {
+            GameObject gb = null;
+            if (isLaunch)
+            {
+                gb = Instantiate(gravitiBall, launchPoint.transform);
+                Destroy(gb, 10f);
+            }
+            yield return new WaitForSeconds(2f);
+            Vector2 vec = Random.Range(-1, 1f) * Vector2.right;
+            vec += Random.Range(-1, 1f) * Vector2.up;
+            gb.GetComponent<Rigidbody2D>().AddForce(vec * launchPow, ForceMode2D.Impulse);
+        }
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Launch());
+    }
+}
