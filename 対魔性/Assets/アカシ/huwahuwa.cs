99eb6b4011a7bdeaeac6dca64ee69e8977b3b6c5
poru0423 <46467086+poru0423@users.noreply.github.com>
2020-04-06 13:56:19 +0900

    ふわふわするかもしれない(仮）

---
+++ 対魔性/Assets/アカシ/huwahuwa.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class huwahuwa : MonoBehaviour
+{
+    public float huwa = .2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.position = new Vector3(transform.position.x
+         , 10 + Mathf.Sin(Time.frameCount * huwa), transform.position.z);
+
+    }
+}
