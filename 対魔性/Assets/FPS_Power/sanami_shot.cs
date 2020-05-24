2acacdb9a3853b9a71ef4addeebfa7bc304153e1
poru0423 <46467086+poru0423@users.noreply.github.com>
2020-05-24 19:20:01 +0900

    さなみ攻撃手段追加(弾オブジェクトなし)

---
+++ 対魔性/Assets/FPS_Power/sanami_shot.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sanami_shot : MonoBehaviour
+{
+    public float span = 3f;
+    public float Dest_time = 5.0f;
+    private float currentTime = 0f;
+    public GameObject boul;
+    public float speed = 1000;
+
+
+    private void OnBecameInvisible()
+    {
+
+        enabled = false;
+    }
+    private void OnBecameVisible()
+    {
+        enabled = true;
+
+    }
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (enabled == true)
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime > span)
+            {
+
+                GameObject bullets = Instantiate(boul) as GameObject;
+                Vector3 force;
+                force = this.gameObject.transform.forward * speed;
+                bullets.GetComponent<Rigidbody2D>().AddForce(force);
+                Destroy(bullets, Dest_time);
+                currentTime = 0f;
+
+            }
+
+        }
+        else
+        {
+            Debug.Log("画面外");
+        }
+    }
+}
+
+
