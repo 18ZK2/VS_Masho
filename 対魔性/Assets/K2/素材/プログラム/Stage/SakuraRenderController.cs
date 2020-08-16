dceefd5853fc244a9c1776a2d6448f7f064f977d
FtoyCimpOmnkHty <55353405+FtoyCimpOmnkHty@users.noreply.github.com>
2020-08-16 21:00:05 +0900

    3面づくり
    背景を曇りにした

--- 対魔性/Assets/K2/素材/プログラム/Stage/SakuraRenderController.cs
+++ 対魔性/Assets/K2/素材/プログラム/Stage/SakuraRenderController.cs
@@ -6,31 +6,29 @@ using UnityEngine.UI;
 public class SakuraRenderController : MonoBehaviour
 {
     ParticleSystem ps;
+    ParticleSystemRenderer pr;
     Renderer r;
+
     // Start is called before the first frame update
     void Start()
     {
         ps = GetComponent<ParticleSystem>();
-        r = GetComponent<Renderer>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
     
-            if (r.isVisible)
-            {
-                var e = ps.emission;
-                e.enabled = true;
-                //ps.emission = e;
-                Debug.Log("表示中");
-            }
-            else
-            {
-                var e = ps.emission;
-                e.enabled = false;
-                Debug.Log("非表示中");
-            }
+            
+    }
+    private void OnBecameInvisible()
+    {
+        var e = ps.emission;
+        e.enabled = false;
+    }
+    private void OnBecameVisible()
+    {
+        var e = ps.emission;
+        e.enabled = true;
     }
 }
