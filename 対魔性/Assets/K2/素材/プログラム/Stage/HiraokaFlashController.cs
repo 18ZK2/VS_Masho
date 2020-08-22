1899ab9bd69790edab4b5233ee032d17aa773da1
FtoyCimpOmnkHty <55353405+FtoyCimpOmnkHty@users.noreply.github.com>
2020-08-22 23:17:40 +0900

    平岡軽量化

--- 対魔性/Assets/K2/素材/プログラム/Stage/HiraokaFlashController.cs
+++ 対魔性/Assets/K2/素材/プログラム/Stage/HiraokaFlashController.cs
@@ -8,38 +8,53 @@ public class HiraokaFlashController : MonoBehaviour
     [SerializeField] float distance = 750f;
     [SerializeField] Tile on = null, off = null;
 
-
+    float camSize;
     Transform player;
     Tilemap map;
     BoundsInt bound;
     IEnumerator flashJudge()
     {
+        /*Debug.Log("Y");
+        Debug.Log(bound.max.y);
+        Debug.Log(bound.min.y);
+        Debug.Log("X");
+        Debug.Log(bound.max.x);
+        Debug.Log(bound.min.x);*/
         while (player != null)
         {
-            for (int y = bound.max.y; y >= bound.min.y; --y)
+            Vector3 pPos = player.transform.position;
+            int yMin = (int)(pPos.y - camSize) / 16;
+            int yMax = (int)(pPos.y + camSize) / 16;
+            int xMin = (int)(pPos.x - camSize) / 16;
+            int xMax = (int)(pPos.x + camSize) / 16;
+            //Debug.Log("YMin Ymax" + yMin.ToString() + yMax.ToString());
+            //Debug.Log("XMin YMax" + xMin.ToString() + xMax.ToString());
+            for (int y = yMin; y < yMax; y++)
             {
-                for (int x = bound.min.x; x < bound.max.x; x++)
+                for (int x = xMin; x < xMax; x++)
                 {
                     Vector3Int tilePos = new Vector3Int(x, y, 0);
                     Vector3 tp = new Vector3(x * 16, y * 16, 0);
                     bool t = map.HasTile(tilePos);
                     if (t)
-                    { 
-                        
+                    {
+
                         float diff = (player.position - tp).magnitude;
                         Tile tile = (diff < distance) ? on : off;
                         map.SetTile(tilePos, tile);
                         //tile.sprite = 
                     }
                 }
+                yield return new WaitForSeconds(0.0025f);
             }
-            yield return new WaitForSeconds(0.1f);
+            
         }
     }
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player").transform;
+        camSize = Camera.main.orthographicSize;
         map = GetComponent<Tilemap>();
         //タイルマップがある範囲を取得
         bound = map.cellBounds;
