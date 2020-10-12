c5ac9804b07f95982fedc3538e3efefdc63fff2c
FtoyCimpOmnkHty <55353405+FtoyCimpOmnkHty@users.noreply.github.com>
2020-10-13 07:56:20 +0900

    バリア　効果音

---
+++ 対魔性/Assets/K2/素材/プログラム/Player/BarrierSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (AudioSource))]
+public class BarrierSound : MonoBehaviour
+{
+    [SerializeField] AudioClip se;
+    bool played = false;
+    AudioSource ass;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ass = GetComponent<AudioSource>();
+        ass.playOnAwake = false;
+        ass.volume = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (played == false)
+        {
+            ass.PlayOneShot(se);
+            played = true;
+        }
+    }
+    private void OnDisable()
+    {
+        played = false;
+        Debug.Log("aaa");
+    }
+}
